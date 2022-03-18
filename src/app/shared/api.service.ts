3b99b16391d9c0956c1f2d00a6979295f8bddfeb
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-18 10:25:34 +0300

    Staff Application

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,16 @@ import {HttpClient} from "@angular/common/http";
 export class ApiService {
 
   constructor(private _http: HttpClient) { }
+
+  postEmployee(data:any){
+    return this._http.post<any>("http://localhost:3000/posts", data).pipe(
+      map(
+        (
+          res => {
+            return res;
+          }
+        )
+      )
+    )
+  }
 }
