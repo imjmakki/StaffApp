569ed299733fb576b902eab00e531cf01d21f952
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-18 10:33:32 +0300

    Staff Application

--- src/app/layout/board/board.component.ts
+++ src/app/layout/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-board',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
   }
